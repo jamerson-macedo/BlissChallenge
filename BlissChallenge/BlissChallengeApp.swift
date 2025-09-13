a984eccd022e2405738eff0788e736d2c6ea50c0
Jamerson Macedo <jamerson_macedo@hotmail.com>
2025-09-13 14:00:54 -0300

    Initial Commit

---
+++ BlissChallenge/BlissChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlissChallengeApp.swift
+//  BlissChallenge
+//
+//  Created by Jamerson Macedo on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BlissChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
