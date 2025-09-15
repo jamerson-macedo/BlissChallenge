aab0ade8a192636f7647538f8f81393d8e4e7f0a
Jamerson Macedo <jamerson_macedo@hotmail.com>
2025-09-15 19:56:21 -0300

    config Avatar for swiftdata

---
+++ BlissChallenge/Model/Avatar.swift
@@ -0,0 +1,30 @@
+//
+//  Emoji 2.swift
+//  BlissChallenge
+//
+//  Created by Jamerson Macedo on 15/09/25.
+//
+
+
+import Foundation
+import SwiftData
+
+@Model
+class Avatar: Identifiable, Equatable {
+    var login: String
+    var avatarURL: String
+    @Attribute(.externalStorage)
+    var imageData: Data?
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarURL = "avatar_url"
+        case imageData
+    }
+    init(login: String, avatarURL: String, imageData: Data? = nil) {
+        self.login = login
+        self.avatarURL = avatarURL
+        self.imageData = imageData
+    }
+}
+
+
